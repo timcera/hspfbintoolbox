0cb2dd598e1a11a9c8a7ac2568db890f510f3cad
Tim Cera <tim@cerazone.net>
2015-07-15 16:02:46 -0400

    Correct version for tstoolbox in setup.py.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
     'baker >= 1.3',
     'python-dateutil >= 2.1',    # python-dateutil-2.0 is for Python 3.0
     'pandas >= 0.9.0',
-    'tstoolbox >= 0.8.5',
+    'tstoolbox >= 0.9.7',
 ]
 
 
