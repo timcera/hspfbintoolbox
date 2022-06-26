32cb574e8e1f93f1f20c6a15120a87b03e62ef21
Sourcery AI <>
2022-06-26 22:30:29 +0000

    'Refactored by Sourcery'

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -48,21 +48,14 @@ def read_unicode_csv(
 ):
     # Python 3 version
     if sys.version_info[0] >= 3:
-        # Open the file in text mode with given encoding
-        # Set newline arg to ''
-        # (see https://docs.python.org/3/library/csv.html)
-        # Next, get the csv reader, with unicode delimiter and quotechar
-        csv_reader = csv.reader(
+        yield from csv.reader(
             filename,
             delimiter=delimiter,
             quotechar=quotechar,
             quoting=quoting,
             lineterminator=lineterminator,
         )
-        # Now, iterate over the (already decoded) csv_reader generator
-        for row in csv_reader:
-            yield row
-    # Python 2 version
+
     else:
         # Next, get the csv reader, passing delimiter and quotechar as
         # bytestrings rather than unicode
